5314c1e8ddbdf4a79f5d96995dffb1130489fa12
hyoenju <l67378810@gmail.com>
2016-12-22 22:56:31 +0900

    link

--- book_project/book_project/urls.py
+++ book_project/book_project/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/index.html'), name='Index'),
     url(r'^login$', TemplateView.as_view(template_name='pages/login.html'), name='login'),
     url(r'^sign_in$', TemplateView.as_view(template_name='pages/sing_in.html'), name='sign_in'),
+    url(r'^market$', TemplateView.as_view(template_name='pages/market.html'), name='market'),
+    url(r'^report_center$', TemplateView.as_view(template_name='pages/report_center.html'), name='report_center'),
+    url(r'^hope_book$', TemplateView.as_view(template_name='pages/hope_book.html'), name='hope_book'),
+    url(r'^gong9$', TemplateView.as_view(template_name='pages/gong9.html'), name='gong9'),
 ]
